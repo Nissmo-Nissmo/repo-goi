9f19f610c88ebbcdec189646ff76ef1d0010c06c
Mikle <voroninmihail1@mail.ru>
2021-01-21 12:43:41 +0700

    Добавьте файлы проекта.

---
+++ калькулятор/Source.cpp
@@ -0,0 +1,25 @@
+#include <iostream>;
+#include <string>;
+
+
+using namespace std;
+int main()
+
+{
+	float a, b;
+
+	cout << "Inter number 1\n";
+	cin >> a;
+	cout << "inter number 2\n";
+	cin >> b;
+	cout << endl;
+
+	
+	
+	cout << a << "/" << b << "=" << a / b << endl;
+	cout << a << "+" << b << "=" << a + b << endl;
+	cout << a << "*" << b << "=" << a * b << endl;
+
+		
+		return 0;
+}
